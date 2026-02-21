97632e6bd52477a8f242486e6b39d764e47c6acd
cjos <flasblack509@gmail.com>
2026-02-20 22:06:57 -0300

    template dp django para o formulario

--- crud/views.py
+++ crud/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,redirect
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from .forms import loginForm
 
-
+@login_required(login_url = '/auth/login')
 def login_view(request):
-    return render(request,'crud/login.html')
+    form = loginForm()
+    return render(request,'crud/login.html',{'form':form})
 
 
 def home_view(request):
